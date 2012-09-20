07fc1b3c16635ab107795b461efa4e4eb86e245d
MichaelHsu <evenchange4@gmail.com>
2012-09-20 22:39:24 +0800

    123

---
+++ jsonaddid.rb
@@ -0,0 +1,30 @@
+
+require 'open-uri'
+require "nokogiri"
+require "json"
+require 'geocoder'
+
+a= open('mrt_pretty.json', "rb").read
+
+input_json = JSON.parse(a)
+anArray = Array.new 	#放album的array
+
+count = 1
+input_json.each do|node|
+	tempHash = Hash.new	#每ID的hash
+	puts '____'
+	puts tempHash['id'] = count
+	puts tempHash['name'] = node['number']
+	puts tempHash['address'] = node['address']
+	puts tempHash['latitude'] = node['latitude']
+	puts tempHash['longitude'] = node['longitude']
+
+	anArray << tempHash
+	count = count+1
+end
+
+puts 'total MRT numbers: '+anArray.count.to_s
+
+f = File.open("mrt2_pretty.json", 'w')
+f.write(JSON.pretty_generate(anArray))
+f.close()
