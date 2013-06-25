85a856e046a2751ba6549c1f92aef2dd0f5bbca3
MichaelHsu <evenchange4@gmail.com>
2013-06-25 11:08:00 +0800

    correct

--- convertformat.rb
+++ convertformat.rb
@@ -1,17 +1,27 @@
+# encoding: UTF-8
 require "json"
 
-result = Hash.new([])
+result = Hash.new()
 
 f = File.open("mrt.json").read
 JSON.parse(f).each do |mrt|
 	blo = mrt["address"][8..10]
+	# p blo = mrt["address"][0..4]
 
 	lat = mrt["latitude"]
 	lng = mrt["longitude"]
 
-	result[blo] = result[blo] << {"lat"=> lat, "lng"=> lng}
+	if result.include?(blo)
+		result[blo] = result[blo] << {"lat"=> lat, "lng"=> lng}
+	else
+		result[blo] = [{"lat"=> lat, "lng"=> lng}]
+	end
+
+
 end
 
+t = JSON.pretty_generate(result)
+
 nf = File.open("mrt_convert.json", "w")
-nf << result.to_json
+nf << t
 nf.close
